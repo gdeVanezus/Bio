49e9036133c3312a1efacbda3c636261ec64d723
gdeVanezus <gdeVanezus@gmail.com>
2025-01-04 14:34:23 +0200

    🍃 Update packages version + Prettier

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
 const Card = ({ children }) => {
   return (
     <motion.div
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8, ease: 'easeOut' }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
       className="relative"
     >
       {children}
